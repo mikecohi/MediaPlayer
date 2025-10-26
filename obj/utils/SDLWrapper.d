9d8ba7c583f521bb118d5f96a6c8a01fbef43c0a
dunggx03 <duonganhdung169@gmail.com>
2025-10-26 23:45:22 +0700

    temp

--- obj/utils/SDLWrapper.d
+++
@@ -1,2 +0,0 @@
-obj/utils/SDLWrapper.o: src/utils/SDLWrapper.cpp src/utils/SDLWrapper.h
-src/utils/SDLWrapper.h:
--- obj/view/SidebarView.d
+++
@@ -1,5 +0,0 @@
-obj/view/SidebarView.o: src/view/SidebarView.cpp src/view/SidebarView.h \
- src/utils/NcursesUI.h src/view/UIManager.h
-src/view/SidebarView.h:
-src/utils/NcursesUI.h:
-src/view/UIManager.h:
