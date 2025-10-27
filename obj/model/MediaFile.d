56b51d2caabc46277fe927057c21a61fb81c516e
dunggx03 <duonganhdung169@gmail.com>
2025-10-27 19:10:10 +0700

    usb view 1

--- obj/model/MediaFile.d
+++
@@ -1,6 +0,0 @@
-obj/model/MediaFile.o: src/model/MediaFile.cpp src/model/MediaFile.h \
- src/model/Metadata.h src/model/AudioMetadata.h src/model/VideoMetadata.h
-src/model/MediaFile.h:
-src/model/Metadata.h:
-src/model/AudioMetadata.h:
-src/model/VideoMetadata.h:
