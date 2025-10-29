b7cb0e434464a253cfcf8fcd5e030da969f36669
dunggx03 <duonganhdung169@gmail.com>
2025-10-29 14:26:58 +0700

    usb fix 4

---
+++ obj/model/Playlist.d
@@ -0,0 +1,8 @@
+obj/model/Playlist.o: src/model/Playlist.cpp src/model/Playlist.h \
+ src/model/MediaFile.h src/model/Metadata.h src/model/AudioMetadata.h \
+ src/model/VideoMetadata.h
+src/model/Playlist.h:
+src/model/MediaFile.h:
+src/model/Metadata.h:
+src/model/AudioMetadata.h:
+src/model/VideoMetadata.h:
---
+++ obj/utils/USBUtils.d
@@ -0,0 +1,2 @@
+obj/utils/USBUtils.o: src/utils/USBUtils.cpp src/utils/USBUtils.h
+src/utils/USBUtils.h:
